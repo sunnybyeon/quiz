bc3c5bc6de2b9a4c697ae2fbd1b85d06bac832bd
sunnybyeon <sunnybyeon2631@gmail.com>
2021-07-18 13:55:00 +0900

    퀴즈 번호 묻는 것 선택 사항으로 변경, 정답 개수 확인 기능 추가

--- quiz.js
+++ quiz.js
@@ -1,9 +1,10 @@
 window.onload = function() {
     class Quiz_Arr {
-        constructor(id, firstA, secA) {
+        constructor(id, firstA, secA, askNum) {
             this.id = id;
             this.firstA = firstA;
             this.secA = secA;
+            this.askNum = askNum;
             // add Event
             document.getElementById(id).getElementsByClassName("startBtn")[0].addEventListener("click", function() {
                 const obj = eval(this.parentElement.getAttribute("id"));
@@ -32,36 +33,64 @@ window.onload = function() {
             const endResult = endSec.getElementsByClassName("endResult")[0];
             const endBtn = endSec.getElementsByClassName("endBtn")[0];
             
+            // 정답 개수 세는 변수
+            let correct = 0;
+            // 0부터 시작되는 숫자로 문제 배열 길이만큼 긴 배열 생성
             const numA = Array.from(Array(this.firstA.length).keys());
-            function  setDisplay(appSecD, questSecD, resultSecD, endSecD) {
+
+            function  setDisplay(appSecD, questSecD, resultSecD, endSecD, otherQuizD) {
                 const appSec = document.getElementById(obj.id).getElementsByClassName("appSec")[0];
                 appSec.style.display = appSecD;
                 appSec.getElementsByClassName("questSec")[0].style.display = questSecD;
                 appSec.getElementsByClassName("resultSec")[0].style.display = resultSecD;
                 appSec.getElementsByClassName("endSec")[0].style.display = endSecD;
+                const allQuiz = Array.from(document.getElementsByTagName("body")[0].children);
+                allQuiz.forEach(element => {
+                    if (element.getAttribute("id") !== obj.id) {
+                        element.style.display = otherQuizD;
+                    }
+                })
             }
+
             function  startQ() {
                 startBtn.style.display = "none";
-                setDisplay("block", "block", "none", "none");
+                setDisplay("block", "block", "none", "none", "none");
             }
+
             function makeQ() {
-                setDisplay("block", "block", "none", "none");
+                setDisplay("block", "block", "none", "none", "none");
                 const n = Math.floor(Math.random() * (numA.length));
                 reply01.value = "";
-                reply02.value = "";
+                if (obj.askNum) {
+                    reply02.value = "";
+                }
                 question.innerHTML = obj.firstA[numA[n]];
                 numA.splice(n, 1);
             }
+
             function checkF() {
-                setDisplay("block", "none", "block", "none");
+                setDisplay("block", "none", "block", "none", "none");
                 const n = obj.firstA.indexOf(question.innerHTML);
-                if (Number(reply01.value) === (n + 1) && reply02.value === obj.secA[n]) {
-                    answer.style.display = "none";
-                    result.innerHTML = "정답입니다!";
+                if (obj.askNum) {
+                    if (Number(reply01.value) === (n + 1) && reply02.value === obj.secA[n]) {
+                        answer.style.display = "none";
+                        result.innerHTML = "정답입니다!";
+                        correct++;
+                    } else {
+                        answer.style.display = "block";
+                        result.innerHTML = "오답입니다."
+                        answer.innerHTML = "정답 : " + String(n + 1) + " , " + obj.secA[n];
+                    }
                 } else {
-                    answer.style.display = "block";
-                    result.innerHTML = "오답입니다."
-                    answer.innerHTML = "정답 : " + String(n + 1) + " , " + obj.secA[n];
+                    if (reply01.value === obj.secA[n]) {
+                        answer.style.display = "none";
+                        result.innerHTML = "정답입니다!";
+                        correct++;
+                    } else {
+                        answer.style.display = "block";
+                        result.innerHTML = "오답입니다."
+                        answer.innerHTML = "정답 : "  + obj.secA[n];
+                    }
                 }
                 if (numA.length !== 0) {
                     nextBtn.innerHTML = "다음 문제";
@@ -69,6 +98,7 @@ window.onload = function() {
                     nextBtn.innerHTML = "결과 보기";
                 }
             }
+
             function nextQ() {
                 if (numA.length !== 0) {
                     makeQ();
@@ -76,14 +106,17 @@ window.onload = function() {
                     showResultQ();                    
                 }
             }
+
             function showResultQ() {
-                setDisplay("block", "none", "none", "block");
-                endResult.innerHTML = "문제가 끝났습니다.";
+                setDisplay("block", "none", "none", "block", "none");
+                endResult.innerHTML = String(correct) + "/" + String(obj.firstA.length);
             }
+
             function endQ() {
-                setDisplay("none", "none", "none", "none");
+                setDisplay("none", "none", "none", "none", "block");
                 startBtn.style.display = "block";
             }
+
             checkBtn.addEventListener("click", checkF);
             nextBtn.addEventListener("click", nextQ);
             endBtn.addEventListener("click", endQ);
@@ -94,5 +127,5 @@ window.onload = function() {
 
     const korE = ["수소", "헬륨", "리튬", "베릴륨"];
     const engE = ["H", "He", "Li", "Be"];
-    const elementSymbol = new Quiz_Arr("elementSymbol", korE, engE);
+    const elementSymbol = new Quiz_Arr("elementSymbol", korE, engE, true);
 }
